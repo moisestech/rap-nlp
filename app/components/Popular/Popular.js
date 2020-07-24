a710304af9f38149c9c5f71a1c45cfc98c5ca485
Moises Sanabria <sesmoi.data@gmail.com>
2020-07-24 11:48:27 -0400

    caching fetch Github response

--- app/components/Popular/Popular.js
+++ app/components/Popular/Popular.js
@@ -45,26 +45,35 @@ export default class Popular extends React.Component {
   updateLanguage (selectedLanguage) {
     this.setState({
       selectedLanguage,
-      error: null,
-      repos: null
+      error: null
     })
 
-    fetchPopularRepos(selectedLanguage)
-      .then((repos) => this.setState({
-        repos,
-        error: null
-      }))
-      .catch(() => {
-        console.warn('Error fetching repos: ', error)
-
-        this.setState({
-          error: 'There was an error fetching the repositories'
+    if (!this.state.repost[selectedLanguage]) {
+      fetchPopularRepos (selectedLanguage)
+        .then((data) => {
+          this.setState(({ repos }) => ({
+            repos: {
+              repos,
+              [selectedLanguage]:data
+            }
+          }))
         })
-      })
+        .catch(() => {
+            console.warn('Error fetching repos: ', error)
+
+            this.setState({
+              error: 'There was an error fetching the repositories'
+            })
+          })
+    }
+
+
   }
 
   isLoading() {
-    return this.state.repos === null && this.state.error === null
+    const { selectedLanguage, repos, error } = this.state
+
+    return !repos[selectedLanguage] && error === null
   }
   render() {
     const { selectedLanguage, repos, error } = this.state
@@ -80,7 +89,7 @@ export default class Popular extends React.Component {
 
         {error && <p>{error}</p>}
 
-        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
       </React.Fragment>
     )
   }
