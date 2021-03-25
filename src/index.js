bfa798d01449a7863473637c8c2a1f52f1eab6f4
moisestech <m@moises.tech>
2021-03-25 05:39:22 -0400

    refactored unused components

--- src/index.js
+++ src/index.js
@@ -11,14 +11,6 @@ import Batches from "./components/Batches";
 
 import CreatePhrase from "./components/CreatePhrase";
 
-import NavUidev from "./components/Uidev/NavUidev";
-import NavHooks from "./components/Uidev/Hooks/NavHooks";
-import NavUseState from "./components/Uidev/Hooks/NavUseState";
-import NavUseEffect from "./components/Uidev/Hooks/NavUseEffect";
-import NavCustomHooks from "./components/Uidev/Hooks/NavCustomHooks";
-import WaitDelay from "./components/Uidev/Hooks/UseEffect/WaitDelay";
-import CharLimit from "./components/Uidev/Hooks/UseEffect/CharLimit";
-
 // Component
 // State
 // Lifecycle
@@ -39,25 +31,6 @@ class App extends React.Component {
       <Router>
         <ThemeProvider value={this.state}>
           <TopNav />
-
-          <Route path="/uidev" component={NavUidev} />
-          <Route path="/uidev/hooks" component={NavHooks} />
-          <Route path="/uidev/hooks/useState" component={NavUseState} />
-          <Route path="/uidev/hooks/useEffect" component={NavUseEffect} />
-          <Route path="/uidev/hooks/customHook" component={NavCustomHooks} />
-
-          <div className="content">
-            <Route
-              exact
-              path="/uidev/hooks/useEffect/practice/char-limit"
-              component={CharLimit}
-            />
-            <Route
-              exact
-              path="/uidev/hooks/useEffect/practice/wait-delay"
-              component={WaitDelay}
-            />
-          </div>
 
           <Switch>
             <Route exact path="/" component={Batch} />
