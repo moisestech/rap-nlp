a3367d4e0c82b6a62ae630465f87dd188fa9f42b
moisestech <m@moises.tech>
2021-03-30 05:21:26 -0400

    init prettier refactor

--- src/components/CreatePhrase.js
+++ src/components/CreatePhrase.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 /*
   INSTRUCTIONS:
@@ -11,29 +11,32 @@ function generateId() {
   return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-function Todo () {
-  const [todos, setTodos] = useState([])
-  const [input, setInput] = useState('')
-
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
 
   const handleSubmit = (evt) => {
-    setTodos((todos) => todos.concat({
-      text: input,
-      id: generateId()
-    }))
+    setTodos((todos) =>
+      todos.concat({
+        text: input,
+        id: generateId(),
+      })
+    );
 
-    setInput('')
-  }
+    setInput('');
+  };
 
-  const removeTodo = (id) => setTodos((todos) => todos.filter((t) => t.id !== id))
+  const removeTodo = (id) =>
+    setTodos((todos) => todos.filter((t) => t.id !== id));
 
   return (
     <div>
       <input
         type="text"
         value={input}
-        placeholder='New Todo'
-        onChange={(e) => setInput(e.target.value)} />
+        placeholder="New Todo"
+        onChange={(e) => setInput(e.target.value)}
+      />
       <button onClick={handleSubmit}>Submit</button>
 
       <ul>
@@ -49,92 +52,93 @@ function Todo () {
 }
 
 class InputPhrase extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-      phrase: ''
-    }
+      phrase: '',
+    };
 
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    this.props.onSubmit(this.state.word)
+    this.props.onSubmit(this.state.word);
 
-    console.log(this.state.phrase)
+    console.log(this.state.phrase);
   }
   handleChange(event) {
     this.setState({
-      phrase: event.target.value
-    })
+      phrase: event.target.value,
+    });
   }
   render() {
     return (
-      <form className='column player' onSubmit={this.handleSubmit}>
-        <label htmlFor='username' className='player-label'>
+      <form className="column player" onSubmit={this.handleSubmit}>
+        <label htmlFor="username" className="player-label">
           {this.props.label}
         </label>
-        <div className='row player-inputs'>
+        <div className="row player-inputs">
           <input
-            type='text'
-            id='phrase'
-            className='input-light'
-            placeholder='add phrase'
-            autoComplete='off'
+            type="text"
+            id="phrase"
+            className="input-light"
+            placeholder="add phrase"
+            autoComplete="off"
             value={this.state.phrase}
             onChange={this.handleChange}
           />
           <button
-            className='btn dark-btn'
-            type='submit'
+            className="btn dark-btn"
+            type="submit"
             disabled={!this.state.phrase}
           >
             Submit
           </button>
         </div>
       </form>
-    )
+    );
   }
 }
 
 InputPhrase.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired
-}
+  label: PropTypes.string.isRequired,
+};
 
 export default class CreatePhrase extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       phrases: [],
-      batch: false
-    }
+      batch: false,
+    };
 
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleReset = this.handleReset.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
   handleSubmit(id, phrase) {
     this.setState({
-      [id]: phrase
-    })
+      [id]: phrase,
+    });
   }
   handleReset() {
     this.setState({
-      [id]: null
-    })
+      [id]: null,
+    });
   }
   render() {
     return (
-      <div className='players-container'>
-          <h1 className='center-text header-lg'>Create Phrase</h1>
+      <div className="players-container">
+        <h1 className="center-text header-lg">Create Phrase</h1>
         <InputPhrase
-          label='Phrase'
-          onSubmit={(phrase) => this.handleSubmit('phrase', phrase)}/>
+          label="Phrase"
+          onSubmit={(phrase) => this.handleSubmit('phrase', phrase)}
+        />
       </div>
-    )
+    );
   }
 }
