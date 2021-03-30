abecc96167aa8c515c0b982a48e9707debe49e99
moisestech <m@moises.tech>
2021-03-30 05:20:56 -0400

    init prettier refactor

--- src/components/Nav/TopNav.js
+++ src/components/Nav/TopNav.js
@@ -1,83 +1,87 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Modal from '../Modal'
-import RightNav from './RightNav'
-import { ThemeConsumer } from '../../contexts/theme'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from '../Modal';
+import RightNav from './RightNav';
+import { ThemeConsumer } from '../../contexts/theme';
+import { NavLink } from 'react-router-dom';
 
 const activeStyle = {
-  color: 'rgb(187, 46, 31)'
-}
+  color: 'rgb(187, 46, 31)',
+};
 
-function PhonemeNav ({selectedPhonemeTag, phonemeColor, onUpdatePhonemeTag}) {
-  const phonemeTags = ['All','AH', 'EH', 'EEH', 'OH', 'OOH']
+function PhonemeNav({ selectedPhonemeTag, phonemeColor, onUpdatePhonemeTag }) {
+  const phonemeTags = ['All', 'AH', 'EH', 'EEH', 'OH', 'OOH'];
 
   return (
-    <ul className='flex-center phoneme-nav'>
+    <ul className="flex-center phoneme-nav">
       {phonemeTags.map((phonemeTag, index) => (
-          <li key={index}>
-            <button
-              className='btn-clear nav-link'
-              style={phonemeTag === selectedPhonemeTag ? {color: phonemeColor} : null}
-              onClick={() => onUpdatePhonemeTag(phonemeTag)}>
-              {phonemeTag}
-            </button>
-          </li>
-        ))}
+        <li key={index}>
+          <button
+            className="btn-clear nav-link"
+            style={
+              phonemeTag === selectedPhonemeTag ? { color: phonemeColor } : null
+            }
+            onClick={() => onUpdatePhonemeTag(phonemeTag)}
+          >
+            {phonemeTag}
+          </button>
+        </li>
+      ))}
     </ul>
-  )
+  );
 }
 
 PhonemeNav.propTypes = {
   selectedPhonemeTag: PropTypes.string.isRequired,
   phonemeColor: PropTypes.string,
-  onUpdatePhonemeTag: PropTypes.func.isRequired
-}
+  onUpdatePhonemeTag: PropTypes.func.isRequired,
+};
 
 export default class TopNav extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       selectedPhonemeTag: 'All',
-      phonemeColor: null
-    }
+      phonemeColor: null,
+    };
 
-    this.updatePhonemeTag = this.updatePhonemeTag.bind(this)
+    this.updatePhonemeTag = this.updatePhonemeTag.bind(this);
   }
   updatePhonemeTag(selectedPhonemeTag) {
-    let phonemeColor
+    let phonemeColor;
 
-    const phonemeColorType =  (selectedPhonemeTag === 'All') ? phonemeColor = null :
-                              (selectedPhonemeTag === 'AH') ? phonemeColor = 'magenta' :
-                              (selectedPhonemeTag === 'EH') ? phonemeColor = 'red' :
-                              (selectedPhonemeTag === 'EEH') ? phonemeColor = 'green' :
-                              (selectedPhonemeTag === 'OH') ? phonemeColor = 'gold' :
-                              (selectedPhonemeTag === 'OOH') ? phonemeColor = 'blue': null
+    const phonemeColorType =
+      selectedPhonemeTag === 'All'
+        ? (phonemeColor = null)
+        : selectedPhonemeTag === 'AH'
+        ? (phonemeColor = 'magenta')
+        : selectedPhonemeTag === 'EH'
+        ? (phonemeColor = 'red')
+        : selectedPhonemeTag === 'EEH'
+        ? (phonemeColor = 'green')
+        : selectedPhonemeTag === 'OH'
+        ? (phonemeColor = 'gold')
+        : selectedPhonemeTag === 'OOH'
+        ? (phonemeColor = 'blue')
+        : null;
 
     this.setState({
       selectedPhonemeTag,
-      phonemeColor
-    })
+      phonemeColor,
+    });
   }
   render() {
-    const { selectedPhonemeTag,
-            phonemeColor } = this.state
+    const { selectedPhonemeTag, phonemeColor } = this.state;
 
     return (
       <ThemeConsumer>
         {({ theme, toggleTheme }) => (
-          <div className='top-nav'>
-            <NavLink
-              to='/batches'
-              exact
-              className='nav-link'>
+          <div className="top-nav">
+            <NavLink to="/batches" exact className="nav-link">
               Posts
             </NavLink>
-            <NavLink
-              to='/create-phrase'
-              exact
-              className='nav-link'>
+            <NavLink to="/create-phrase" exact className="nav-link">
               Create Phrase
             </NavLink>
             <PhonemeNav
@@ -86,8 +90,8 @@ export default class TopNav extends React.Component {
               onUpdatePhonemeTag={this.updatePhonemeTag}
             />
             <button
-              style={{fontSize: 30}}
-              className='btn-clear'
+              style={{ fontSize: 30 }}
+              className="btn-clear"
               onClick={toggleTheme}
             >
               {theme === 'light' ? '🔦' : '💡'}
@@ -96,6 +100,6 @@ export default class TopNav extends React.Component {
           </div>
         )}
       </ThemeConsumer>
-    )
+    );
   }
 }
